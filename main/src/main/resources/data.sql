a55a2d949be909cd018cbe204eed4c6f692cb9e9
varunp <59484133+varunpshrivathsa@users.noreply.github.com>
2021-10-25 18:41:38 +0530

    Create data.sql

---
+++ main/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE inventory;
+INSERT INTO inventory (sku, price, qty) VALUES ('Orange', 10.00, 100);
+INSERT INTO inventory (sku, price, qty) VALUES ('Apple', 10.00, 100);
+INSERT INTO inventory (sku, price, qty) VALUES ('Kiwi', 10.00, 100);
+INSERT INTO inventory (sku, price, qty) VALUES ('Bananas', 10.00, 0);
