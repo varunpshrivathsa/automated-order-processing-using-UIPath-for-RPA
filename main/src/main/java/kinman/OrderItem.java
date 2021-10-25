eb3d5154beb572f2073eed0c74566839b39180cd
varunp <59484133+varunpshrivathsa@users.noreply.github.com>
2021-10-25 18:38:39 +0530

    Create OrderItem.java

---
+++ main/src/main/java/kinman/OrderItem.java
@@ -0,0 +1,77 @@
+package kinman;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Order order;
+
+    private String sku;
+
+    private BigDecimal price;
+
+    private int qty;
+
+    private BigDecimal extPrice;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getExtPrice() {
+        return extPrice;
+    }
+
+    public void setExtPrice(BigDecimal extPrice) {
+        this.extPrice = extPrice;
+    }
+}
